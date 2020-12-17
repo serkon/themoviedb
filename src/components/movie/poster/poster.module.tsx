310673bbdcd71fc9c8e4d7e4dce630009fc3474e
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2020-12-17 03:08:53 +0300

    style: poster component style improvment

--- src/components/movie/poster/poster.module.tsx
+++ src/components/movie/poster/poster.module.tsx
@@ -7,7 +7,7 @@ export const MoviePosterComponent = ({ poster, className }: { poster: MoviePoste
   return (
     <div className={`${styles.host} ${className}`}>
       <img className={styles.path} src={`${api.image.path}${api.image.size}${poster.poster_path}`} />
-      <div className={styles.raiting}><span className={styles.percent}>{poster.vote_average * 10}<sup>%</sup></span></div>
+      <div className={styles.raiting}><span className={styles.percent}>{poster.vote_average * 10}<sup className={styles.sub}>%</sup></span></div>
       <div className={styles.content}>
         <h2 className={styles.title}>
           <Link href="movie/detail">
